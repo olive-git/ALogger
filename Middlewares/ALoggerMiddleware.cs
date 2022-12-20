bb18beb1ed8aed3c00970f3fa91836bf11541c8f
mengolo.olive@gmail.com <mengolo.olive@gmail.com>
2022-12-20 16:14:50 +0100

    Added new middleware to listen to incoming request

---
+++ Middlewares/ALoggerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using ALogger.Services.Interfaces;
+using ALogger.Tools.Interfaces;
+
+namespace ALogger.Logger.Middlewares
+{
+    public class ALoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ALoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            IDisplay._request = context.Request;
+
+            //var request = context.Request;
+            //var query = request.Query;
+            //var content = query["content"].ToString();
+            //var id = query["id"].ToString();
+
+            //Console.WriteLine($" Request id : {request}");
+
+            //if (content != null || content != "")
+            //    Console.WriteLine($" Content : {content}");
+            //if (content != null || content != "")
+            //    Console.WriteLine($" Id : {id}");
+
+            //foreach (var header in request.Headers)
+            //{
+            //    Console.WriteLine($" HEADER : {header}");
+            //}
+
+            await _next(context);
+        }
+        
+    }
+
+    public static class ALoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseALoggerMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ALoggerMiddleware>();
+        }
+    }
+}
--- Services/Implementation/Logger.cs
+++ Services/Implementation/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using ALogger.Constantes;
+using ALogger.Logger.Middlewares;
 using ALogger.Services.Interfaces;
 using ALogger.Tools.Implementations;
 using System;
@@ -12,14 +13,21 @@ using AppName = System.Reflection.Assembly;
 
 namespace ALogger.Services.Implementation
 {
+
+    /// <summary>
+    /// Cette classe définit les fonctions pour logger
+    /// </summary>
     public class Logger : ILogger
     {
+
+        #region Properties
         private readonly Display _Log;
         private readonly int _log_ranking;
         private string _className = new StackFrame(1).GetMethod().DeclaringType.Name;
         private string _appName = AppName.GetEntryAssembly().GetName().Name;
+        #endregion
 
-
+        #region Constructors
         public Logger(string level = "Err")
         {
             _Log = new Display();
@@ -33,7 +41,9 @@ namespace ALogger.Services.Implementation
                 _log_ranking = LogLevel.log_level_ranking["Err"];
             }
         }
+        #endregion
 
+        #region Methods
         public string logDebug(string message, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string memberName = "")
         {
             string log_level = LogLevel.log_level["Debug"];
@@ -70,6 +80,7 @@ namespace ALogger.Services.Implementation
             int log_level_ranking = LogLevel.log_level_ranking[log_level];
             DateTime date = DateTime.Now;
 
+            
             if (log_level_ranking <= _log_ranking)
             {
                 _Log.display(date, log_level, message, _className, _appName, lineNumber, memberName);
@@ -108,5 +119,6 @@ namespace ALogger.Services.Implementation
             string log = $"{date} : {log_level} : {message}";
             return log;
         }
+        #endregion
     }
 }
