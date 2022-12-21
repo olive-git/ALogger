ab0650562752414bd74e14a40073edf6fe6703b1
mengolo.olive@gmail.com <mengolo.olive@gmail.com>
2022-12-21 14:07:09 +0100

    removed variable 'variable'

--- Services/Implementation/Logger.cs
+++ Services/Implementation/Logger.cs
@@ -25,7 +25,6 @@ namespace ALogger.Services.Implementation
         private readonly int _log_ranking;
         private string _className = new StackFrame(1).GetMethod().DeclaringType.Name;
         private string _appName = AppName.GetEntryAssembly().GetName().Name;
-        string variable;
         #endregion
 
         #region Constructors
